a00924a0e0b2b4da24160ef5a7b0a91391475bdf
Mookiepiece <1779834460@qq.com>
2019-09-11 21:13:10 +0800

    1

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import { render } from "react-dom";
 import './default.css';
 import select from 'select';
+// import SparkMD5 from 'spark-md5';
+//console.log(SparkMD5.hash("sp"));
 
 window.onload = () => {
-render(<ControlPanel />, root);
+    render(<ControlPanel />, root);
 }
 
 class ControlPanel extends React.Component {
